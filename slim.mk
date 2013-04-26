0462949818aec41b84c37015e850365111b7b734
elelinux <ele.linux@gmail.com>
2013-04-26 18:37:35 +0200

    Remove cm and add slim instead

--- slim.mk
+++ slim.mk
@@ -1,22 +1,28 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := pyramid
 
-TARGET_BOOTANIMATION_NAME := vertical-540x960
+# Preload bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/pyramid/device_pyramid.mk)
 
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
 # Device naming
 PRODUCT_DEVICE := pyramid
-PRODUCT_NAME := cm_pyramid
+PRODUCT_NAME := slim_pyramid
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := Sensation
+PRODUCT_MODEL := HTC Sensation
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.

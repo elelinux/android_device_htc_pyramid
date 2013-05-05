10aee65ab5446c158926095edc6f8f8d97d126aa
elelinux <ele.linux@gmail.com>
2013-05-05 13:47:49 +0200

    Removed 99kernel script and added sweep2wake instead

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -61,7 +61,7 @@ PRODUCT_COPY_FILES += \
 # Some misc configuration files
 PRODUCT_COPY_FILES += \
     device/htc/pyramid/vold.fstab:system/etc/vold.fstab \
-    device/htc/pyramid/configs/99kernel:system/etc/init.d/99kernel
+    device/htc/pyramid/configs/sweep2wake:system/etc/init.d/sweep2wake
 
 # Keylayouts and Keychars
 PRODUCT_COPY_FILES += \
